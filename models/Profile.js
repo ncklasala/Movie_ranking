542fa459582d3a3577a95e9e801b35ec87f21dbe
ncklasala <33462642+ncklasala@users.noreply.github.com>
2020-07-05 17:00:40 -0400

    Adjusted profile to fit my use case

--- models/Profile.js
+++ models/Profile.js
@@ -8,32 +8,16 @@ const ProfileSchema = new Schema({
     ref: "users",
   },
 
-  handle: {
+  screenName: {
     type: String,
     required: true,
     max: 40,
   },
-  company: {
-    type: String,
-  },
-  website: {
-    type: String,
-  },
-  location: {
-    type: String,
-  },
-  status: {
-    type: String,
-    required: true,
-  },
-  skills: {
+  favoriteGenres: {
     type: [String],
     required: true,
   },
   bio: {
-    type: String,
-  },
-  githubusername: {
     type: String,
   },
   experience: [
@@ -41,17 +25,6 @@ const ProfileSchema = new Schema({
       title: { type: String, required: true },
       company: { type: String, required: true },
       location: { type: String },
-      from: { type: Date, required: true },
-      to: { type: Date },
-      current: { type: Boolean, default: false },
-      description: { type: String },
-    },
-  ],
-  education: [
-    {
-      school: { type: String, required: true },
-      degree: { type: String, required: true },
-      fieldOfStudy: { type: String, required: true },
       from: { type: Date, required: true },
       to: { type: Date },
       current: { type: Boolean, default: false },
